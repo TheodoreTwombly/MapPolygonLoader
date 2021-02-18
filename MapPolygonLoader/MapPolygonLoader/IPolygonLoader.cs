b2e3a608a5558b24a810e20beea9e8e2ec20bf14
burdin_d <daniil-b96@mail.ru>
2021-02-18 12:46:44 +0300

    add interface IPolygonLoader and class Polygon, Point

---
+++ MapPolygonLoader/MapPolygonLoader/IPolygonLoader.cs
@@ -0,0 +1,13 @@
+﻿using MapPolygonLoader.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MapPolygonLoader
+{
+    public interface IPolygonLoader
+    {
+        Polygon GetPolygonPoints(string address, int frequencyOfPoints);
+    }
+}
---
+++ MapPolygonLoader/MapPolygonLoader/Models/Polygon.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MapPolygonLoader.Models
+{
+    public class Polygon
+    {
+        public string Name { get; set; }
+        public List<Point> Points { get; set; }
+
+    }
+}
